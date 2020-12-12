ef206ca1581353bd43acaf1f5ebb073287798647
Emily Ng <ngemily2@gmail.com>
2020-12-12 13:24:12 -0500

    12 pt2

--- 12/main.py
+++ 12/main.py
@@ -1,10 +1,13 @@
-import pudb # noqa
+from collections import namedtuple
+import pudb  # noqa
+
+Point = namedtuple("Point", ["x", "y"])
 
 with open("input.txt") as f:
     lines = f.readlines()
 
 
-def parse_instruction(instruction, x, y, z):
+def parse_instruction1(instruction, x, y, z):
     action, value = instruction[0], instruction[1:]
 
     def rotate(orientation, degrees, direction):
@@ -37,11 +40,48 @@ def parse_instruction(instruction, x, y, z):
     return func_map[action](int(value))
 
 
+def parse_instruction(instruction, ship, waypoint):
+    action, value = instruction[0], instruction[1:]
+
+    def rotate_waypoint(waypoint, degrees, direction):
+        if direction == "L":
+            degrees = 360 - degrees
+        func_map = {
+            90: lambda: (Point(waypoint.y, -waypoint.x)),
+            180: lambda: (Point(-waypoint.x, -waypoint.y)),
+            270: lambda: (Point(-waypoint.y, waypoint.x)),
+        }
+        return func_map[degrees]()
+
+    def advance_ship(ship, waypoint, n):
+        x_delta, y_delta = n * waypoint.x, n * waypoint.y
+        return Point(ship.x + x_delta, ship.y + y_delta), waypoint
+
+    func_map = {
+        "N": lambda n: (ship, Point(waypoint.x, waypoint.y + n)),
+        "E": lambda n: (ship, Point(waypoint.x + n, waypoint.y)),
+        "S": lambda n: (ship, Point(waypoint.x, waypoint.y - n)),
+        "W": lambda n: (ship, Point(waypoint.x - n, waypoint.y)),
+        "R": lambda n: (ship, rotate_waypoint(waypoint, n, "R")),
+        "L": lambda n: (ship, rotate_waypoint(waypoint, n, "L")),
+        "F": lambda n: advance_ship(ship, waypoint, n),
+    }
+    return func_map[action](int(value))
+
+
 x = 0
 y = 0
 orientation = "E"
 
 instructions = map(lambda line: line.strip(), lines)
 for instruction in instructions:
-    x, y, orientation = parse_instruction(instruction, x, y, orientation)
+    x, y, orientation = parse_instruction1(instruction, x, y, orientation)
 print(abs(x) + abs(y))
+
+ship = Point(0, 0)
+waypoint = Point(10, 1)
+
+instructions = map(lambda line: line.strip(), lines)
+for instruction in instructions:
+    ship, waypoint = parse_instruction(instruction, ship, waypoint)
+print(abs(ship.x) + abs(ship.y))
